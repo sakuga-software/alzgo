298e9baff869bb9a77e29b171bea5c63a17e8aae
Mathieu Audebert <mathieu.adbrt@gmail.com>
2025-01-16 11:37:10 +0100

    build: update vite config to remove hash from build file names

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,24 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    /**
+     * This ensures that the output files are builded without the hash.
+     */
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+        assetFileNames: (chunkInfo) => {
+          if (chunkInfo.names?.[0]?.endsWith('.svg')) {
+            return '[name].[ext]';
+          }
+
+          return '[name][extname]';
+        },
+        manualChunks: undefined,
+        compact: true,
+      },
+    },
+  },
   plugins: [react()],
 });
